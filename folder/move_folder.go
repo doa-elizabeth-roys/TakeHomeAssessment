f04764e1f71d49869f213bf201f76634bb8d081e
Doa Roys <doaelizabethroys@gmail.com>
2024-11-15 22:45:13 +1000

    Find the source and destination folders

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -1,7 +1,36 @@
 package folder
 
-func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
-	// Your code here...
+import (
+	"errors"
+	// "strings"
+)
 
-	return []Folder{}, nil
+func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
+	var srcFolder *Folder
+	var dstFolder *Folder
+
+	// Find source folder
+	for i := range f.folders {
+		if f.folders[i].Name == name {
+			srcFolder = &f.folders[i]
+			break
+		}
+	}
+	if srcFolder == nil {
+		return nil, errors.New("source folder does not exist")
+	}
+
+	// Find destination folder
+	for i := range f.folders {
+		if f.folders[i].Name == dst {
+			dstFolder = &f.folders[i]
+			break
+		}
+	}
+	if dstFolder == nil {
+		return nil, errors.New("destination folder does not exist")
+	}
+
+	// Return the updated folder structure
+	return f.folders, nil
 }
